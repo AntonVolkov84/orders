c122cfd3e1c861e37aed97772b515005693ea258
Anton Volkov <antvolkov84@gmail.com>
2024-11-10 16:43:40 +0200

    third try

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Alert, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RegistrationScreen from "./screens/RegistrationScreen";
 import LoginScreen from "./screens/LoginScreen";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect, useRef } from "react";
 import DashboardScreen from "./screens/DashboardScreen.jsx";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import OrderScreen from "./screens/OrderScreen.jsx";
 import MessagingScreen from "./screens/MessagingScreen.jsx";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { registerForPushNotificationsAsync } from "./notifications.js";
 
 const Stack = createNativeStackNavigator();
 export const AppContext = createContext(null);
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Original Title",
+    body: "And here is the body! Hahaha",
+    data: { someData: "goes here" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
 
 export default function App() {
   const [user, setUser] = useState("");
   const [participants, setParticipants] = useState("");
   const auth = getAuth();
+  const [notification, setNotification] = useState(null);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error) => setExpoPushToken(`${error}`));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.current && Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current && Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   onAuthStateChanged(auth, (user) => {
     if (user && auth.currentUser.emailVerified) {
       setUser(user);
--- notifications.js
+++ notifications.js
@@ -1,4 +1,6 @@
 import * as Notifications from "expo-notifications";
+import { getMessaging, getToken } from "firebase/messaging";
+import Constants from "expo-constants";
 
 async function registerForPushNotificationsAsync() {
   let token;
@@ -12,19 +14,21 @@ async function registerForPushNotificationsAsync() {
     alert("Failed to get push token for push notification!");
     return;
   }
-  token = (await Notifications.getExpoPushTokenAsync()).data;
-  console.log("token", token);
-
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
+  const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+  if (!projectId) {
+    handleRegistrationError("Project ID not found");
   }
-
-  return token;
+  try {
+    const pushTokenString = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId,
+      })
+    ).data;
+    console.log(pushTokenString);
+    return pushTokenString;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export { registerForPushNotificationsAsync };
